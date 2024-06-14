d1ad06c36c6f9661a4157cbce9d00e6f57d0ca69
Arnav Srivastava <arnavsrivastava214@gmail.com>
2024-06-14 13:37:25 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+    const routes: Routes = [
+      {
+        path:'signIn',
+        loadChildren: ()=>import('./sign-in/sign-in.module').then(m => m.SignInModule)
+
+      },{
+        path:'signUp',
+        loadChildren:()=>import('./sign-up/sign-up.module').then(m=>m.SignUpModule)
+      },
+      {
+        path:'newUser',
+        loadChildren:()=>import ("./new-user/new-user.module").then(m=>m.NewUserModule)
+      }
+      
+    ];
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    NewUserComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+export class AppModule {
+  
+ }
